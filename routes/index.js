be23dccd6da6972822f8657326dd676e7501c0ac
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-13 22:06:46 +0100

    created views

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const data = [
-    {
-        message: "this is a message",
-    }
-];
-
 router.get('/', (req, res) => {
-    res.json(data);
+    res.send('Hello world');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_URL !== 'production') require('dotenv').config();
 // brew services stop mongodb-community@4.4 --> to stop mongodb service
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
@@ -13,6 +14,12 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to mongoose'));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 app.use(cors());
 
