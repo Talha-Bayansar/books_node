5e0f0c8c89391b278fbf80c92aceb21bf5662bd0
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-13 19:24:52 +0100

    created routes

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000);
